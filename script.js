7b397893652b92307bd858565a7d09bac8f35689
Marcela <marcelamm6@gmail.com>
2022-09-03 18:54:57 -0300

    the end

--- script.js
+++ script.js
@@ -27,17 +27,23 @@ const linkContainer = resultadoTag.querySelector('#p-link');
 const corContainer = resultadoTag.querySelector('#containerCor');
 const veiculoContainer = resultadoTag.querySelector('#p-veiculo');
 const categoriaContainer = resultadoTag.querySelector('#p-categoria');
+const linkCurto = resultadoTag.querySelector('#link-curto')
 const tagContainer = resultadoTag.querySelector('#tags');
 
 // console.log(linkInput);
 
 linkInput.addEventListener('keyup', e => {
   linkContainer.textContent = e.currentTarget.value;
+  blueParrot.link = linkContainer.textContent;
+  
 });
+
 
 radioButtons.forEach(radioBtn => {
   radioBtn.addEventListener('change', e => {
     corContainer.textContent = e.currentTarget.value;
+    blueParrot.classificação = corContainer.textContent;
+   
   });
 });
 
@@ -46,15 +52,63 @@ veiculos.addEventListener('change', e => {
   let selectedValue = select.value;
   let selectedOption = select.options[selectedValue];
   veiculoContainer.textContent = selectedOption.textContent + ' - ';
+  blueParrot.veiculo = veiculoContainer.textContent;
 });
 categoria.addEventListener('change', e => {
   let select = e.currentTarget;
   let selectedValue = select.value;
   let selectedOption = select.options[selectedValue];
   categoriaContainer.textContent = selectedOption.textContent;
+  blueParrot.categoria = categoriaContainer.textContent
 });
 
 tagInput.addEventListener('keyup', e => {
   tagContainer.textContent = e.currentTarget.value;
+  blueParrot.tags = tagContainer.textContent;
 });
 //teste.addEventListener('keydown')
+
+const encurtar = document.querySelector('.btn-encurtar');
+
+encurtar.addEventListener('click', () => {
+  document.querySelector('.btn-encurtar').style.display = "none"
+  document.querySelector('.carregando').style.display = "block"
+  
+  $.ajax({
+    method: 'POST',
+    url: 'https://api.encurtador.dev/encurtamentos',
+    headers: {
+      'content-type': 'application/json',
+    },
+    data: JSON.stringify( { url: linkInput.value } ) ,
+  }).done(response => {
+    document.querySelector('.btn-encurtar').style.display = "block"
+    document.querySelector('.carregando').style.display = "none"
+    linkCurto.textContent = response.urlEncurtada
+
+    blueParrot.linkCurto = linkCurto.textContent
+
+    // console.log(response.urlEncurtada)
+  }).fail(e => {
+    document.querySelector('.btn-encurtar').style.display = "block"
+    document.querySelector('.carregando').style.display = "none"
+    console.log(e);
+  });
+
+});
+
+
+console.log(blueParrot);
+
+
+const btnCopiar = document.querySelector('.btn-copiar');
+
+
+btnCopiar.addEventListener('click', (e) => {
+  navigator.clipboard.writeText(
+    `${blueParrot.link}\n${blueParrot.classificação}${blueParrot.veiculo}${blueParrot.categoria}\n${blueParrot.linkCurto}\n${blueParrot.tags}`
+  ).then(() => {
+    alert('Copiado')
+  })
+
+})
